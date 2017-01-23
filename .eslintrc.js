649120d529888852d129230acea76bf6a5fd9a46
Rijn <bxbian951122@gmail.com>
2017-01-22 18:14:42 -0600

    fixed ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    'indent': ["error", 4],
+    "semi": ["error", "always"],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
